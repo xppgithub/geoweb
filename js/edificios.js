0bf01743e247f4962ab3791343a7f44ea87f0c3b
Xavier Pedrola Puig <xavierpp12@ttc.cat>
2020-01-22 17:34:20 +0100

    mapbox mod 20200122

--- js/edificios.js
+++ js/edificios.js
@@ -47,7 +47,7 @@ function addEdificiosCapa() {
 
 }
 
-function addPopupToMapEdificios(nombreCapa) {
+function addPopupToMapEdificios2(nombreCapa) {
 
     map.on('click', nombreCapa, function (e) {
   
@@ -95,8 +95,50 @@ function activarEdificios (activo) {
 
     }
 
+}
+
+function addPopupToMapEdificios(nombreCapa) {
+
+  map.on('click', nombreCapa, function (e) {
+
+      var text = "";
+      //console.info(e);
+      for (key in e.features[0].properties) {
+
+          if (key == "numberOfFloorsAboveGround") {
+              text += "<b>Numero de plantas</b>:" + e.features[0].properties[key] + "<br>";
+          }
+          if (key == "localId") {
+              //localId 0004702DF3800C_part1
+              //http://ovc.catastro.meh.es/OVCServWeb/OVCWcfLibres/OVCFotoFachada.svc/RecuperarFotoFachadaGet?ReferenciaCatastral=0004701DF3800C
+              //https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?rc1=0004701&rc2=DF3800C
+
+              var localId = e.features[0].properties[key];
+
+              var localIdSplit = localId.split("_"); // 0004702DF3800C  part1
+              var parte1 = localIdSplit[0].substring(0, 7);
+              var parte2 = localIdSplit[0].substring(7, localIdSplit[0].length);
+              text += "<img width=200 src=http://ovc.catastro.meh.es/OVCServWeb/OVCWcfLibres/OVCFotoFachada.svc/RecuperarFotoFachadaGet?ReferenciaCatastral=" + localId + "><br>";
+              text += "<a target=blank href=https://www1.sedecatastro.gob.es/CYCBienInmueble/OVCListaBienes.aspx?rc1=" + parte1 + "&rc2=" + parte2 + ">Ficha</a><br>";
+
+          }
 
 
+      }
+      new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(text)
+          .addTo(map);
+
+  });
+
+  map.on('mouseenter', nombreCapa, function () {
+      map.getCanvas().style.cursor = 'pointer';
+  });
+
+  map.on('mouseleave', nombreCapa, function () {
+      map.getCanvas().style.cursor = '';
+  });
 
 }
 
